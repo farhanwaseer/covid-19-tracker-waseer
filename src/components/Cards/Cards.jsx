f1f612df953a16487589e668c5a1078360265605
farhanwaseer <farhanaliwaseer@gmail.com>
2020-06-20 14:29:02 -0400

    updated

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid, } from '@material-ui/core';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if(!confirmed) {
+        return 'Loading... ';
+
+    }
+
     return (
-        <h1>Cards</h1>
-    )
+        <div className={styles.container}>
+            <Grid comtainer spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">{confirmed.value}</Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
+    );
 }
 
 export default Cards;
