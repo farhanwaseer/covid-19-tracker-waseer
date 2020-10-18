305be02c8a4a0ee464f67985c3883efef55fa375
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-18 22:17:04 +0500

    ,,.,

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React , {Fragment} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
+import App from './App.js';
+import { Breadcrumbs } from '@material-ui/core';
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Fragment>
+      <Breadcrumbs />
+        <App />
+    </Fragment>   
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+, document.getElementById("root"));
