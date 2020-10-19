8974dd6305980de162dcdcf850e84fa9922d4a63
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-19 23:46:52 +0500

    uudt

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React , {Fragment} from 'react';
-import ReactDOM from 'react-dom';
-// import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
 import App from './App.js';
-import { Breadcrumbs } from '@material-ui/core';
-
-
 
 ReactDOM.render(
     <Fragment>
-      <Breadcrumbs />
-        <App />
+         <App />
     </Fragment>   
 
 , document.getElementById("root"));
