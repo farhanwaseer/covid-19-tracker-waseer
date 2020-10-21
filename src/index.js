e93b8be096a0d9a49733e117ab305b5e73c965cf
farhanwaseer <farhanaliwaseer@gmail.com>
2020-10-21 23:03:45 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@ import React , {Fragment} from 'react';
 import ReactDOM from 'react-dom'
 import App from './App.js';
 
+const element = React.createElement('div', null, 'FARHAN WASEER');
+console.log(element)
+
+const odList = React.createElement('ol', { className: 'odList '}, 
+           React.createElement('li', null, 'Amir'),
+           React.createElement('li', null, 'Irfan'),
+           React.createElement('li', null, 'Muneeb')
+
+
+)
+
+const odCities = ['Karachi', 'lahore', 'Faisalabad', 'Abbottabad'];
+const odcitie = React.createElement('ol', null, 
+    odCities.map((city, idx) => React.createElement('li', {key: idx}, city)) 
+)
+
 ReactDOM.render(
     <Fragment>
          <App />
+         {element}
+         <div>
+             karachi
+         </div>
+         <div>
+             lahore
+         </div>
+         {odList}
+         {odcitie}
     </Fragment>   
 
 , document.getElementById("root"));
